c72375a3e2b61c1c960d88ac7cfb07cd8c93a584
Marcos becker <marcos@logikee.com.br>
2024-10-04 14:33:38 -0300

    feat(main): create base project

---
+++ src/main/java/dev/marcosbd/simple_twitter_backend/SimpleTwitterBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.marcosbd.simple_twitter_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTwitterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTwitterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/marcosbd/simple_twitter_backend/SimpleTwitterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcosbd.simple_twitter_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTwitterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
