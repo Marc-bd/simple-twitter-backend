b5e6e3dfc5580bc2fe0c455c9695e96b0511fede
Marcos becker <marcos@logikee.com.br>
2024-10-15 12:19:23 -0300

    feat(STB-08): implement oauth authorization and admin access

--- src/main/java/dev/marcosbd/simple_twitter_backend/controller/userController/UserController.java
+++ src/main/java/dev/marcosbd/simple_twitter_backend/controller/userController/UserController.java
@@ -2,37 +2,51 @@ package dev.marcosbd.simple_twitter_backend.controller.userController;
 
 
 import dev.marcosbd.simple_twitter_backend.dtos.user.CreateUserDTO;
+import dev.marcosbd.simple_twitter_backend.dtos.user.ResponseCreateUser;
 import dev.marcosbd.simple_twitter_backend.entities.Role;
 import dev.marcosbd.simple_twitter_backend.entities.User;
 import dev.marcosbd.simple_twitter_backend.repositories.RoleRepository;
 import dev.marcosbd.simple_twitter_backend.repositories.UserRepository;
+import dev.marcosbd.simple_twitter_backend.services.userServices.UserService;
+import jakarta.transaction.Transactional;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UserController {
 
-    private UserRepository userRepository;
-    private RoleRepository roleRepository;
+    private UserService userService;
 
-    public UserController(UserRepository userRepository, RoleRepository roleRepository) {
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Transactional
+    @PostMapping("/auth/register")
+    public ResponseEntity<ResponseCreateUser> createUser(@RequestBody
+                                           CreateUserDTO userData) {
+
+
+        ResponseCreateUser newUser = userService.create(userData);
+
+        return ResponseEntity.ok().body(newUser);
     }
 
 
-//    @PostMapping("/users")
-//    public ResponseEntity<User> createUser(@RequestBody
-//                                           CreateUserDTO userData) {
-//
-//        Role userRole = roleRepository.findByName(Role.Values.user.name());
-//
-//
-//
-//        return ResponseEntity.ok()
-//
-//    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAuthority('SCOPE_admin')")
+    public ResponseEntity<List<ResponseCreateUser>> getUsers() {
+        List<ResponseCreateUser> users = userService.getUsers();
+        return ResponseEntity.ok().body(users);
+    }
+
 
 }
--- src/main/java/dev/marcosbd/simple_twitter_backend/services/userServices/UserService.java
+++ src/main/java/dev/marcosbd/simple_twitter_backend/services/userServices/UserService.java
@@ -2,39 +2,63 @@ package dev.marcosbd.simple_twitter_backend.services.userServices;
 
 
 import dev.marcosbd.simple_twitter_backend.dtos.user.CreateUserDTO;
+import dev.marcosbd.simple_twitter_backend.dtos.user.ResponseCreateUser;
 import dev.marcosbd.simple_twitter_backend.entities.Role;
 import dev.marcosbd.simple_twitter_backend.entities.User;
 import dev.marcosbd.simple_twitter_backend.repositories.RoleRepository;
 import dev.marcosbd.simple_twitter_backend.repositories.UserRepository;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserService {
 
     private UserRepository userRepository;
     private RoleRepository roleRepository;
+    private BCryptPasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-//    public User create(CreateUserDTO userData) {
-//        Role userRole = roleRepository.findByName(Role.Values.user.name());
-//
-//        Optional<User> userExist = userRepository.findByEmail(userData.email());
-//
-//        if (userExist.isPresent()) {
-//            throw new DuplicateKeyException("User Already Exists");
-//        }
-//
-//
-//
-//
-//    }
+    public ResponseCreateUser create(CreateUserDTO userData) {
+        Role userRole = roleRepository.findByName(Role.Values.user.name());
 
+        Optional<User> userExist = userRepository.findByEmail(userData.getEmail());
+
+        System.out.println("service : " + userData);
+
+        if (userExist.isPresent()) {
+            throw new DuplicateKeyException("User Already Exists");
+        }
+
+        User newUser = new User(userData, passwordEncoder);
+
+        newUser.setRole(Set.of(userRole));
+
+
+        userRepository.save(newUser);
+
+        return new ResponseCreateUser(newUser);
+    }
+
+    public List<ResponseCreateUser> getUsers() {
+        List<User> users = userRepository.findAll();
+        List<ResponseCreateUser> responseCreateUsers = new ArrayList<>();
+
+        for (User user : users) {
+            responseCreateUsers.add(new ResponseCreateUser(user));
+        }
+
+        return responseCreateUsers;
+    }
 
 }
